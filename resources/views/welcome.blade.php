cd997d754d3571c47ef8b89c2a4ec2325856b5e4
Tanvir1476 <tanvirfarhan07@gmail.com>
2026-01-23 11:52:03 +0600

    Welcome Page

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DIU Accounts Portal</title>
+   <link rel="icon" href="{{ asset('Images/diu logo.png') }}" type="image/png">
+   <link rel="stylesheet" href="{{ asset('style.css') }}">
+   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="welcome">
+        <img src="Images/logo_white.png" alt="DIU Logo" height="40px" width="158px">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Semester Fee Amount</a></li>
+        <li><a href="get token">Get Token</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+
+    <div class="second-div">
+        <img src="Images/logo_white.png" alt="DIU Logo"  width="250px">
+    </div>
+<div class="container">
+    <div class="third-div">
+        <p>Welcome to Accounts Portal</p>
+        <a href="login" class="login-btn">Log In</a>
+    </div>
+</div>
+
+<footer>
+    © 2026 DIU Accounts Portal. All rights reserved. Developed by <span id="footer">Farhan Tanvir</span>
+</footer>
+</body>
+</html>
